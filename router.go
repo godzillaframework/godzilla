8750baed80157b2f1016da200f7640d3eec9d127
krishpranav <krisna.pranav@gmail.com>
2021-11-16 06:17:09 +0530

    router: func

--- router.go
+++ router.go
@@ -41,3 +41,68 @@ func (r *router) releaseCtx(ctx *context) {
 	ctx.requestCtx = nil
 	r.pool.Put(ctx)
 }
+
+func (r *router) handle(method, path string, handlers handlersChain) {
+	if path == "" {
+		panic("path is empty")
+	} else if method == "" {
+		panic("method is empty")
+	} else if path[0] != '/' {
+		panic("path must begin with '/' in path '" + path + "'")
+	} else if len(handlers) == 0 {
+		panic("no handlers provided with path '" + path + "'")
+	}
+
+	if r.trees == nil {
+		r.trees = make(map[string]*node)
+	}
+
+	root := r.trees[method]
+	if root == nil {
+		root = createRootNode()
+		r.trees[method] = root
+	}
+
+	root.addRoute(path, handlers)
+}
+
+func (r *router) allowed(reqMethod, path string, ctx *context) string {
+	var allow string
+
+	pathLen := len(path)
+
+	if (pathLen == 1 && path[0] == '*') || (pathLen > 1 && path[1] == '*') {
+		for method := range r.trees {
+			if method == MethodOptions {
+				continue
+			}
+
+			if allow != "" {
+				allow += ", " + method
+			} else {
+				allow = method
+			}
+		}
+		return allow
+	}
+
+	for method, tree := range r.trees {
+		if method == reqMethod || method == MethodOptions {
+			continue
+		}
+
+		handlers := tree.matchRoute(path, ctx)
+		if handlers != nil {
+			if allow != "" {
+				allow += ", " + method
+			} else {
+				allow = method
+			}
+		}
+	}
+
+	if len(allow) > 0 {
+		allow += ", " + MethodOptions
+	}
+	return allow
+}
