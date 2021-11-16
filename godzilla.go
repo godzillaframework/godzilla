60ff2f4c60d51d894914d531ff29eb1f4a537cb7
krishpranav <krisna.pranav@gmail.com>
2021-11-16 06:19:28 +0530

    godzilla

--- godzilla.go
+++ godzilla.go
@@ -9,3 +9,15 @@ const (
 	GODZILLA
 Listening on %s`
 )
+
+const (
+	defaultCacheSize = 1000
+
+	defaultConcurrency = 256 * 1024
+
+	defaultMaxRequestBodySize = 4 * 1024 * 1024
+
+	defaultMaxRouteParams = 1024
+
+	defaultMaxRequestURLLength = 2048
+)
