089dfd42302e2698ab12bbded7e406b995ad13d3
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 12:26:14 +0530

    customtest: asserts go

--- customtest/asserts.go
+++ customtest/asserts.go
@@ -7,6 +7,7 @@ import (
 
 func assertContains(t *testing.T, expected, actual interface{}) {
 	if reflect.TypeOf(expected) != reflect.TypeOf(actual) {
-		
+		tLog(t, "value type not match %T (expected)\n\n\t != %T (actual)", expected, actual)
+
 	}
 }
