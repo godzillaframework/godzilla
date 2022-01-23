17a5814c976ca7fd957abab24e352e0ffb4a74ab
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 12:22:23 +0530

    customtest: asserts

--- customtest/asserts.go
+++ customtest/asserts.go
@@ -1,1 +1,12 @@
 package customtest
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assertContains(t *testing.T, expected, actual interface{}) {
+	if reflect.TypeOf(expected) != reflect.TypeOf(actual) {
+		
+	}
+}
