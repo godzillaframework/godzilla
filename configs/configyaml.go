4443f467d3e86a66598a6e503b628285dbb3c675
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 12:09:39 +0530

    configs: configyaml

--- configs/configyaml.go
+++ configs/configyaml.go
@@ -1,1 +1,19 @@
 package config
+
+import "gopkg.in/yaml.v2"
+
+func unmarshalYaml(content []byte, v interface{}) error {
+	return yaml.Unmarshal(content, v)
+}
+
+func marshalYaml(v interface{}) (out []byte, err error) {
+	return yaml.Marshal(v)
+}
+
+func marshalYamlString(v interface{}) (out string) {
+	marshal, err := yaml.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	return string(marshal)
+}
