f8f76d40169ba8675459054fcbc320d63cd7bead
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-23 12:27:14 +0530

    customtest: assert

--- customtest/asserts.go
+++ customtest/asserts.go
@@ -2,12 +2,24 @@ package customtest
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
 func assertContains(t *testing.T, expected, actual interface{}) {
 	if reflect.TypeOf(expected) != reflect.TypeOf(actual) {
 		tLog(t, "value type not match %T (expected)\n\n\t != %T (actual)", expected, actual)
-
+	}
+	switch e := expected.(type) {
+	case string:
+		a := actual.(string)
+		if !strings.Contains(a, e) {
+			tLog(t, "   %v (expected)\n\n\t!= %v (actual)",
+				expected, actual)
+			t.FailNow()
+		}
+	default:
+		tLog(t, "unsupported type %T(expected)", expected)
+		t.FailNow()
 	}
 }
